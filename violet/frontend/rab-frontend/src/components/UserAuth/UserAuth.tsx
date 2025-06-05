4c53972e7d006e4f5a0e15ef297cac3fe3963ac6
dehazecontrast <andylecontact@gmail.com>
2025-06-04 20:34:53 -0400

    User auth screen further implementation

--- violet/frontend/rab-frontend/src/components/UserAuth/UserAuth.tsx
+++ violet/frontend/rab-frontend/src/components/UserAuth/UserAuth.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './UserAuth.css';
 import password_icon from '../../assets/lock_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png'
 import mail_icon from '../../assets/mail_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png'
 import user_icon from '../../assets/person_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png'
 
 const UserAuth = () => {
+
+    const [action, setAction] = useState("Sign Up");
     return (
         <div className='container'>
             <div className="header">
@@ -14,17 +16,23 @@ const UserAuth = () => {
             <div className="inputs">
                 <div className = "input">
                     <img src={user_icon} alt="" />
-                    <input type="text" />
+                    <input type="text" placeholder="Name"/>
                 </div>
                 <div className = "input">
                     <img src={mail_icon} alt="" />
-                    <input type="email" />
+                    <input type="email" placeholder="E-mail"/>
                 </div>
                 <div className = "input">
                     <img src={password_icon} alt="" />
-                    <input type="password" />
+                    <input type="password" placeholder="Password"/>
                 </div>
             </div>
+            <div className="forgot-password">Forgot Password? <span>Click Here</span></div>
+            <div className="submit-container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
+            </div>
+            
         </div>
     );
 };
