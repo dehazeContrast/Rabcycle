e690d319cc5f35913a263cac75e6580976b59979
dehazecontrast <andylecontact@gmail.com>
2025-07-05 22:57:20 -0400

    hardcoded temporary data for hisory page

--- violet/frontend/rab-frontend/src/components/HistoryContainer/HistoryContainer.tsx
+++ violet/frontend/rab-frontend/src/components/HistoryContainer/HistoryContainer.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import HistoryCard from '../HistoryCard/HistoryCard.tsx'
+import  './HistoryContainer.css'
 import waterImage from '../../assets/water.jpg';
 
 
+//Temp hard-coded data to pull from
 const historyData = [
     {
         image: waterImage,
@@ -12,11 +14,36 @@ const historyData = [
         image:waterImage,
         info: 'June 19, 2025',
     },
+        {
+        image:waterImage,
+        info: 'June 18, 2025',
+    },
+        {
+        image:waterImage,
+        info: 'June 17, 2025',
+    },
+        {
+        image:waterImage,
+        info: 'June 16, 2025',
+    },
+        {
+        image:waterImage,
+        info: 'June 15, 2025',
+    },
+        {
+        image:waterImage,
+        info: 'June 14, 2025',
+    },
+        {
+        image:waterImage,
+        info: 'June 13, 2025',
+    },
 ];
 
 const HistoryContainer = () => {
     return (
         <div className="history-container">
+            <title>History</title>
             {historyData.map((entry, index) => (
             <HistoryCard key={index} image={entry.image} info={entry.info}/>
             ))}
